0dcef402baef4f60477d3ce6cd8ab8cd73e6d9bc
C.Manuel Hernández <cmanuelhm87@gmail.com>
2024-10-06 17:04:54 +0200

    btener Héroe por URL

--- src/app/heroes/pages/list-page/list-page.component.ts
+++ src/app/heroes/pages/list-page/list-page.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../../interfaces/hero.interface';
-import { HerosService } from '../../services/heroes.service';
+import { HeroesService } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-list-page',
@@ -9,7 +9,7 @@ import { HerosService } from '../../services/heroes.service';
 export class ListPageComponent implements OnInit {
   public heroes: Hero[] = [];
 
-  constructor(private heroesService: HerosService) {}
+  constructor(private heroesService: HeroesService) {}
 
   ngOnInit(): void {
     this.heroesService.getHeroes().subscribe((heroes) => {
