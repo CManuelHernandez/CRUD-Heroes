f94b70310c9247591372609642e09045f309fe09
C.Manuel Hernández <cmanuelhm87@gmail.com>
2024-10-06 13:05:58 +0200

    Material Sidenav, Toolbar e iconos

--- src/app/heroes/pages/layout-page/layout-page.component.ts
+++ src/app/heroes/pages/layout-page/layout-page.component.ts
@@ -4,4 +4,10 @@ import { Component } from '@angular/core';
   selector: 'app-layout-page',
   templateUrl: './layout-page.component.html',
 })
-export class LayoutPageComponent {}
+export class LayoutPageComponent {
+  public sidebarItems = [
+    { label: 'Listado', icon: 'label', url: './list' },
+    { label: 'Añadir', icon: 'add', url: './new-hero' },
+    { label: 'Buscar', icon: 'search', url: './search' },
+  ];
+}
